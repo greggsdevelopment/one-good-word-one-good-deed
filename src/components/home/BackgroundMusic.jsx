f03edb564ca29c20fb9bbf6f0cec21e3860b6b41
Cody <greggsdevelopment@gmail.com>
2026-05-31 16:43:32 +0000

    File changes

--- src/components/home/BackgroundMusic.jsx
+++ src/components/home/BackgroundMusic.jsx
@@ -13,7 +13,7 @@ export default function BackgroundMusic() {
   const inIframe = checkInIframe();
 
   const VIDEO_ID = 'LPn0KFlbqX8';
-  const src = `https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&loop=1&playlist=${VIDEO_ID}&enablejsapi=1&controls=0&mute=1&playsinline=1`;
+  const src = `https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&loop=1&playlist=${VIDEO_ID}&enablejsapi=1&controls=0&mute=1&playsinline=1&rel=0&modestbranding=1&version=3`;
 
   useEffect(() => {
     if (inIframe) return;
